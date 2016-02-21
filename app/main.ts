841a1afbf543cf4abf28b91e869fd0934ddabd80
Harold Rodriguez <Harold.rodriguez@aztec.com.au>
2016-02-22 08:00:54 +1100

    Added first route

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
 
 bootstrap(AppComponent);
-bootstrap(HeroDetailComponent);
